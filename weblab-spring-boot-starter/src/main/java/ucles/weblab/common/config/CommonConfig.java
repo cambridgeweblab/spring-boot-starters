b4f825214736cb1124d52b255c8507b454d15305
Neale Upstone <neale@nealeupstone.com>
2017-05-23 15:28:09 +0100

    RES-488 Move most CommonConfig beans to @ConditionalOnWebApplication
    ... so that we can use outside of a servlet container

--- weblab-spring-boot-starter/src/main/java/ucles/weblab/common/config/CommonConfig.java
+++ weblab-spring-boot-starter/src/main/java/ucles/weblab/common/config/CommonConfig.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.boot.autoconfigure.web.DispatcherServletAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.MultipartProperties;
 import org.springframework.boot.context.embedded.FilterRegistrationBean;
@@ -50,89 +51,96 @@ import ucles.weblab.common.webapi.exception.CommonControllerExceptionHandler;
  */
 @Configuration
 @AutoConfigureBefore(DispatcherServletAutoConfiguration.class)
-@ComponentScan(basePackageClasses = {CommonControllerExceptionHandler.class, UnhandledExceptionHandler.class})
 @Import({ConfigurableEntitySupport.class, MoreGenericConverters.class, MoreJsr310Converters.class})
-@EnableConfigurationProperties(MultipartProperties.class)
 public class CommonConfig {
-    private static final String X_FORWARDED_PORT = "X-Forwarded-Port";
-    private static final String $_WSSP = "$wssp";
-
-    @Autowired
-   	MultipartProperties multipartProperties = new MultipartProperties();
 
     @Bean
     BuilderProxyFactory builderProxyFactory() {
         return new BuilderProxyFactory();
     }
 
-    /**
-     * Use JerseyMultipartResolver instead of the default StandardServletMultipartResolver or CommonsMultipartResolver
-     * as it can handle base64 Content-Transfer-Encoding.
-     */
-    @Bean(name = DispatcherServlet.MULTIPART_RESOLVER_BEAN_NAME)
-    @ConditionalOnMissingBean(name = DispatcherServlet.MULTIPART_RESOLVER_BEAN_NAME)
-    MultipartResolver multipartResolver() {
-        MultipartConfigElement multipartConfigElement = multipartProperties.createMultipartConfig();;
-        JerseyMultipartResolver jerseyMultipartResolver = new JerseyMultipartResolver();
-        jerseyMultipartResolver.setSizeMax(multipartConfigElement.getMaxRequestSize());
-        jerseyMultipartResolver.setFileSizeMax(multipartConfigElement.getMaxFileSize());
-        return jerseyMultipartResolver;
-    }
+    @Configuration
+    @ConditionalOnWebApplication
+    @ComponentScan(basePackageClasses = {CommonControllerExceptionHandler.class, UnhandledExceptionHandler.class})
+    @EnableConfigurationProperties(MultipartProperties.class)
+    public static class CommonConfigWeb {
+
+        private static final String X_FORWARDED_PORT = "X-Forwarded-Port";
+        private static final String $_WSSP = "$wssp";
+
+        @Autowired
+        MultipartProperties multipartProperties = new MultipartProperties();
+
+        /**
+         * Use JerseyMultipartResolver instead of the default StandardServletMultipartResolver or CommonsMultipartResolver
+         * as it can handle base64 Content-Transfer-Encoding.
+         */
+        @Bean(name = DispatcherServlet.MULTIPART_RESOLVER_BEAN_NAME)
+        @ConditionalOnMissingBean(name = DispatcherServlet.MULTIPART_RESOLVER_BEAN_NAME)
+        MultipartResolver multipartResolver() {
+            MultipartConfigElement multipartConfigElement = multipartProperties.createMultipartConfig();;
+            JerseyMultipartResolver jerseyMultipartResolver = new JerseyMultipartResolver();
+            jerseyMultipartResolver.setSizeMax(multipartConfigElement.getMaxRequestSize());
+            jerseyMultipartResolver.setFileSizeMax(multipartConfigElement.getMaxFileSize());
+            return jerseyMultipartResolver;
+        }
 
 
-    /**
-     * Add an X-Forward-Port header if an $wssp header is present (i.e. if running on Bluemix Liberty for Java).
-     * This is needed for the API to generate links correctly. Liberty already sets
-     * X-Forwarded-Proto and Host correctly.
-     */
-    @Bean
-    @ConditionalOnProperty(name = "bluemix.enable-wssp-filter", matchIfMissing = true)
-    FilterRegistrationBean xForwardedPortFilterRegistration() {
-        FilterRegistrationBean registrationBean = new FilterRegistrationBean();
-        registrationBean.setFilter(new OncePerRequestFilter() {
-            @Override
-            protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
-                if (httpServletRequest.getHeader($_WSSP) != null && httpServletRequest.getHeader(X_FORWARDED_PORT) == null) {
-                    // Wrap the request and add the header
-                    httpServletRequest = new HttpServletRequestWrapper(httpServletRequest) {
-                        List<String> headerNames = null;
+        /**
+         * Add an X-Forward-Port header if an $wssp header is present (i.e. if running on Bluemix Liberty for Java).
+         * This is needed for the API to generate links correctly. Liberty already sets
+         * X-Forwarded-Proto and Host correctly.
+         */
+        @Bean
+        @ConditionalOnProperty(name = "bluemix.enable-wssp-filter", matchIfMissing = true)
+        FilterRegistrationBean xForwardedPortFilterRegistration() {
+            FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+            registrationBean.setFilter(new OncePerRequestFilter() {
+                @Override
+                protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+                    if (httpServletRequest.getHeader($_WSSP) != null && httpServletRequest.getHeader(X_FORWARDED_PORT) == null) {
+                        // Wrap the request and add the header
+                        httpServletRequest = new HttpServletRequestWrapper(httpServletRequest) {
+                            List<String> headerNames = null;
 
-                        @Override
-                        public String getHeader(String name) {
-                            return name.equalsIgnoreCase(X_FORWARDED_PORT)? super.getHeader($_WSSP) : super.getHeader(name);
-                        }
-
-                        @Override
-                        public Enumeration<String> getHeaders(String name) {
-                            return name.equalsIgnoreCase(X_FORWARDED_PORT)? super.getHeaders($_WSSP) : super.getHeaders(name);
-                        }
-
-                        @Override
-                        public int getIntHeader(String name) {
-                            return name.equalsIgnoreCase(X_FORWARDED_PORT)? super.getIntHeader($_WSSP) : super.getIntHeader(name);
-                        }
-
-                        @Override
-                        public Enumeration<String> getHeaderNames() {
-                            if (this.headerNames == null) {
-                                this.headerNames = new ArrayList<>();
-                                final Enumeration<String> e = super.getHeaderNames();
-                                while (e.hasMoreElements()) {
-                                    this.headerNames.add(e.nextElement());
-                                }
-                                this.headerNames.add(X_FORWARDED_PORT);
+                            @Override
+                            public String getHeader(String name) {
+                                return name.equalsIgnoreCase(X_FORWARDED_PORT)? super.getHeader($_WSSP) : super.getHeader(name);
                             }
-                            return Collections.enumeration(headerNames);
-                        }
-                    };
-                }
-                filterChain.doFilter(httpServletRequest, httpServletResponse);
-            }
-        });
-        registrationBean.setName("xForwardedPortFilter");
-        return registrationBean;
-    }
 
+                            @Override
+                            public Enumeration<String> getHeaders(String name) {
+                                return name.equalsIgnoreCase(X_FORWARDED_PORT)? super.getHeaders($_WSSP) : super.getHeaders(name);
+                            }
+
+                            @Override
+                            public int getIntHeader(String name) {
+                                return name.equalsIgnoreCase(X_FORWARDED_PORT)? super.getIntHeader($_WSSP) : super.getIntHeader(name);
+                            }
+
+                            @Override
+                            public Enumeration<String> getHeaderNames() {
+                                if (this.headerNames == null) {
+                                    this.headerNames = new ArrayList<>();
+                                    final Enumeration<String> e = super.getHeaderNames();
+                                    while (e.hasMoreElements()) {
+                                        this.headerNames.add(e.nextElement());
+                                    }
+                                    this.headerNames.add(X_FORWARDED_PORT);
+                                }
+                                return Collections.enumeration(headerNames);
+                            }
+                        };
+                    }
+                    filterChain.doFilter(httpServletRequest, httpServletResponse);
+                }
+            });
+            registrationBean.setName("xForwardedPortFilter");
+            return registrationBean;
+        }
+
+
+    }
 
     @Configuration
     @ConditionalOnClass(EntityManager.class)
