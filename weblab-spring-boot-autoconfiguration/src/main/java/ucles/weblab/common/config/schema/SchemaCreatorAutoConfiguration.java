11ef4cad032cf12eed92bea951fa8f198dc69967
Neale Upstone <neale@nealeupstone.com>
2018-03-19 15:04:13 +0000

    Ensure SchemaCreatorAutoConfig runs after CrossContextAutoConfiguration
    We got this by chance previously, but in Spring Boot 2/Spring 5 we don't

--- weblab-spring-boot-autoconfiguration/src/main/java/ucles/weblab/common/config/schema/SchemaCreatorAutoConfiguration.java
+++ weblab-spring-boot-autoconfiguration/src/main/java/ucles/weblab/common/config/schema/SchemaCreatorAutoConfiguration.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
 import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
 import org.springframework.security.core.context.SecurityContextHolder;
+import ucles.weblab.common.config.xc.CrossContextAutoConfiguration;
 import ucles.weblab.common.i18n.service.LocalisationService;
 import ucles.weblab.common.schema.webapi.ControllerMethodSchemaCreator;
 import ucles.weblab.common.schema.webapi.EnumSchemaCreator;
@@ -27,7 +28,7 @@ import ucles.weblab.common.xc.service.CrossContextConversionService;
 @Configuration
 @ConditionalOnBean({ ObjectMapper.class, CrossContextConversionService.class })
 @ConditionalOnClass({ SecurityContextHolder.class, JsonSchemaFactory.class, SecurityChecker.class, MethodSecurityExpressionHandler.class, ResourceSchemaCreator.class })
-@AutoConfigureAfter(SecurityAutoConfiguration.class)
+@AutoConfigureAfter({SecurityAutoConfiguration.class, CrossContextAutoConfiguration.class})
 public class SchemaCreatorAutoConfiguration {
 
     @Bean
